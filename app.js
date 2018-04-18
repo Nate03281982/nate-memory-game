91a32570551034e367fe6736e2bba592e77b0a31
Nataniel Ben-Ami <Nate0281982@gmail.com>
2018-04-18 10:17:27 +0300

    Shuffle function and call newgameBoard function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// JavaScript source code
+// JavaScript source code variables
 var gameArray = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'];
 var arrayValues = [];
 var carIds = [];
@@ -6,3 +6,22 @@ var cardsClicked = 0;
 var totalMoves = 0;
 var totalSeconds = 0;
 var timerVar;
+
+newgameBoard();
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
