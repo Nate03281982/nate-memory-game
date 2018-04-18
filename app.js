bddf06aa9808a7a603b91e7f09bcd9f8558c5b3e
Nataniel Ben-Ami <Nate0281982@gmail.com>
2018-04-18 10:11:04 +0300

    define variables in Javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+var gameArray = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'];
+var arrayValues = [];
+var carIds = [];
+var cardsClicked = 0;
+var totalMoves = 0;
+var totalSeconds = 0;
+var timerVar;
